f45d893763ca474dd3a40ef7953596d63c7c64ba
niko-bikchen <niko.bikcheb@gmail.com>
2020-03-30 01:23:34 +0300

    Added comments

--- task_6/lagrange_interpolation.py
+++ task_6/lagrange_interpolation.py
@@ -2,18 +2,27 @@ import numpy as np
 from sympy import Symbol, lambdify, simplify
 import math
 
+# <Test data 1>
+# x_values = np.array([0, 20, 40, 60, 80, 100], float)
+# y_values = np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float)
+# </Test data 1>
+
+# <Test data 2>
+x_values = np.array([-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1], float)
+y_values = np.array([math.atan(2 * val) for val in x_values], float)
+
+
+# </Test data 2>
+
+
+# <Main part>
 
 def lagrange_interpolation(xp, xs, ys):
     return sum(yi * np.prod((xp - xs[xs != xi]) / (xi - xs[xs != xi])) for xi, yi in zip(xs, ys))
 
 
-# Test set 1
-# x_values = np.array([0, 20, 40, 60, 80, 100], float)
-# y_values = np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float)
-
-# Test set 2
-x_values = np.array([-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1], float)
-y_values = np.array([math.atan(2 * val) for val in x_values], float)
+# UNCOMMENT LINES BELOW TO INPUT VALUES
+# ! BE SURE TO COMMENT ALL TEST RELATED LINES
 
 # num = int(input('Number of points: '))
 #
@@ -28,17 +37,26 @@ x = Symbol('x')
 polynomial = simplify(lagrange_interpolation(x, x_values, y_values))
 print(polynomial)
 
-# For tests
+# </Main part>
+
+# <Tests>
+
+# Produces a polynomial, where you can plug 'x', from the 'polynomial' above
+# Used for testing purposes
 P = lambdify(x, polynomial)
 
-# For test set 1
+# <Test set 1>
 # print(f'For x = 0: {P(0)}')
 # print(f'For x = 20: {P(20)}')
 # print(f'For x = 100: {P(100)}')
 # print(f'For x = 40: {P(40)}')
+# </Test set 1>
 
-# For test set 2
+# <Test set 2>
 print(f'For x = -0.9: {P(-0.9)}')
 print(f'For x = -0.3: {P(-0.3)}')
 print(f'For x = 0.3: {P(0.3)}')
 print(f'For x = 0.9: {P(0.9)}')
+# </Test set 2>
+
+# </Tests>
