127008a52c5cf5757b1659a0622e64824072baa2
niko-bikchen <niko.bikcheb@gmail.com>
2020-04-05 21:16:00 +0300

    Chebyshev interpolation v2.0 Fixed some errors

--- task_7/chebyshev_interpolation.py
+++ task_7/chebyshev_interpolation.py
@@ -17,12 +17,12 @@ def a_sum(n, f, k):
         [
             f(
                 cos(
-                    (((2 * j) + 1) * pi) / ((2 * n) + 2)
+                    (2 * j + 1) * pi / (2 * n + 2)
                 )
             )
             *
             cos(
-                (k * ((2 * j) + 1) * pi) / ((2 * n) + 2)
+                k * (2 * j + 1) * pi / (2 * n + 2)
             )
             for j in range(0, n + 1)
         ]
@@ -35,7 +35,7 @@ def custom_a_sum(n, values, k):
             val
             *
             cos(
-                (k * ((2 * j) + 1) * pi) / ((2 * n) + 2)
+                k * (2 * j + 1) * pi / (2 * n + 2)
             )
             for j, val in enumerate(values)
         ]
@@ -48,9 +48,9 @@ def chebyshev_interpolation(n, f, x):
     for i in range(1, n + 1):
 
         if i == 1:
-            summa += ((a_sum(n, f, 1) * cheb_poli(1, x)) / (n + 1))
+            summa += a_sum(n, f, 1) * cheb_poli(1, x) / (n + 1)
         else:
-            summa += ((2 * a_sum(n, f, i) * cheb_poli(i, x)) / (n + 1))
+            summa += 2 * a_sum(n, f, i) * cheb_poli(i, x) / (n + 1)
 
     return summa
 
@@ -61,13 +61,20 @@ def custom_chebyshev_interpolation(n, values, x):
     for i in range(1, n + 1):
 
         if i == 1:
-            summa += ((custom_a_sum(n, values, 1) * cheb_poli(1, x)) / (n + 1))
+            summa += custom_a_sum(n, values, 1) * cheb_poli(1, x) / (n + 1)
         else:
-            summa += ((2 * custom_a_sum(n, values, 1) * cheb_poli(i, x)) / (n + 1))
+            summa += 2 * custom_a_sum(n, values, 1) * cheb_poli(i, x) / (n + 1)
 
     return summa
 
 
+# f_values = [-1.1009971305064690925, -1.0471975511965977461,
+#             -0.90970073709819939446, -0.59993424850586212036,
+#             0, 0.59993424850586212036, 0.90970073709819939446,
+#             1.0471975511965977461, 1.1009971305064690925]
+# num = len(f_values)
+
+# UNCOMMENT LINES BELOW TO INPUT NODE VALUES BY HAND
 # f_values = []
 # num = int(input('Number of nodes: '))
 #
@@ -75,14 +82,9 @@ def custom_chebyshev_interpolation(n, values, x):
 #     f_values.append(float(input(f'Enter value of function in node number {i}: ')))
 #
 # x_val = Symbol('x')
-# polinomial = simplify(custom_chebyshev_interpolation(num, f_values, x_val))
+# polinomial = simplify(custom_chebyshev_interpolation(num - 1, f_values, x_val))
 # print(polinomial)
 
 x_val = Symbol('x')
-# x_val = -0.9
-polinomial = simplify(chebyshev_interpolation(9, lambda arg: atan(arg * 2), x_val))
+polinomial = simplify(chebyshev_interpolation(8, lambda arg: atan(arg * 2), x_val))
 print(polinomial)
-
-# x_val = Symbol('x')
-# polinomial = simplify(cheb_poli(6, x_val))
-# print(polinomial)
